4c03ef1c977cfb01b1d002fbfb224a62ff4a89f1
Nguyễn An <annguyen04@hotmail.com>
2026-06-11 11:23:30 +0700

    fix: ensure grid items stretch equally and properly pass down flex height to prevent WebScrcpy canvas from collapsing to 0px

--- src/components/features/OtpRentBox.jsx
+++ src/components/features/OtpRentBox.jsx
@@ -579,7 +579,7 @@ export default function OtpRentBox() {
 
       {/* SECTION 3: RENT FORM */}
       {activeTab === "rent" ? (
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 items-start">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 items-stretch">
 
           {/* Cột Trái: Order Form & Sound Panel */}
           <div className="lg:col-span-7 flex flex-col gap-6">
@@ -826,7 +826,7 @@ export default function OtpRentBox() {
           {/* End of Left Column */}
 
           {/* Cột Phải: Screen Mirroring */}
-          <div className="lg:col-span-5">
+          <div className="lg:col-span-5 flex flex-col">
             <WebScrcpy />
           </div>
 
